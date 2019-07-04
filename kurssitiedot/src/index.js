8f1ff2f042c8ed976a557858c439c8ab2aef9c26
Marko <majaana@live.com>
2019-07-04 15:42:36 +0300

    step 1.5

--- kurssitiedot/src/index.js
+++ kurssitiedot/src/index.js
@@ -4,32 +4,35 @@ import ReactDOM from 'react-dom';
 
 
 const App = () =>{
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        }, 
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14 
-        }
-    ]
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            }, 
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14 
+            }
+        ]
     
+    } 
+
     let summa = 0
-    const part = parts.map(par => <li key = {par.name}>{par.name + " " +  par.exercises}</li>)
+    const part = course.parts.map(par => <li key = {par.name}>{par.name + " " +  par.exercises}</li>)
     
-    parts.map(p => summa += p.exercises)
+    course.parts.map(p => summa += p.exercises)
     
     return(
         
         <div>
+            <h2>{course.name}</h2>
             <p>
-                <h2>{course}</h2>
                 {part}
                 Number of exercises: {summa}
             </p>
